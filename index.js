f9e016bc1e619962082536b596a2eebd94da091c
febryardiansyah <febrymuhammad80@gmail.com>
2020-07-22 13:00:04 +0700

    i

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const chapter = require('./routers/chapter')
 const comic = require('./routers/comic')
 const cors = require('cors')
 
+app.use(cors())
 app.use('/api',manga,require('./routers/handleError'))
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
-app.use(cors())
 app.use('/api',comic)
 app.use('/api/chapter',chapter)
 app.listen(PORT, function () {
