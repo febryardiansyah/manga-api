d6c7ccb3e68cc848cf59d5ba95f459587c055c61
febryardiansyah <febrymuhammad80@gmail.com>
2020-08-02 22:09:11 +0700

    added download chapter link 🥳

--- routers/chapter.js
+++ routers/chapter.js
@@ -6,11 +6,21 @@ const { default: Axios } = require('axios')
 router.get('/',(req,res) => {
     res.send('chapter')
 })
+
 //chapter ----done ----
 router.get('/:slug', (req, res,next) => {
     const slug = req.params.slug
     const url = baseUrl+slug
     console.log(url);
+    let link
+    Axios.get(`https://pdf.komiku.co.id/${slug}`).then(response => {
+        const $ = cheerio.load(response.data)
+        const element = $('.title')
+        link = element.find('a').attr('href')
+    }).catch(err=>{
+        console.log(err.message);
+    })
+
     Axios.get(url).then(response => {
             const $ = cheerio.load(response.data)            
             const content = $('#article')
@@ -20,16 +30,32 @@ router.get('/:slug', (req, res,next) => {
             content.find('.dsk2').filter(function (){
                 title = $(this).find('h1').text().replace('Komik ','')
             })
+            download_link = link
             content.find('.bc').filter(function (){
                 $(this).find('img').each(function (i,el){
                 chapter_image_link = $(el).attr('src')
                 image_number = i+1
                 chapter_image.push({image_number,chapter_image_link})
                 })
-
             })
             res.json({title,chapter_endpoint,download_link,chapter_image})
+    }).catch(err=>{
+        console.log(err.message);
     })
-    
 })
+
+// router.get('/download/:id',(req, res)=>{
+//     const id = req.params.id
+//     const url = `${baseUrl}download/${id}`
+//     console.log(url);
+//     Axios.get(url).then(response =>{
+//         const $ = cheerio.load(response.data)
+//         const element = $('#fulldl')
+//         let download_pdf
+//         download_pdf = element.find('a').attr('href')
+//         console.log(element.text());
+//         res.json({download_pdf})
+//     })
+// })
+
 module.exports = router
