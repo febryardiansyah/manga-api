0c56c5cdb21292b3283125ee8895a6da3c50812c
febryardiansyah <febrymuhammad80@gmail.com>
2020-08-15 18:18:38 +0700

    refactoring code

--- routers/chapter.js
+++ routers/chapter.js
@@ -33,35 +33,33 @@ router.get("/:slug", async (req, res) => {
     const element = pdf$(".title");
     link = element.find("a").attr("href");
     console.log(link);
-    const response = await AxiosService(slug)
+    const response = await AxiosService(slug);
     const $ = cheerio.load(response.data);
-      const content = $("#article");
-      let chapter_image = [];
-      let title,
-        chapter_image_link,
-        image_number,
-        chapter_endpoint,
-        download_link;
-      chapter_endpoint = slug + "/";
-      content.find(".dsk2").filter(function () {
-        title = $(this).find("h1").text().replace("Komik ", "");
-      });
-      download_link = link;
-      content.find(".bc").filter(function () {
-        $(this)
-          .find("img")
-          .each(function (i, el) {
-            chapter_image_link = $(el).attr("src");
-            image_number = i + 1;
-            chapter_image.push({ image_number, chapter_image_link });
-          });
-      });
-      res.json({ title, chapter_endpoint, download_link, chapter_image });
+    const content = $("#article");
+    let chapter_image = [];
+    let title,
+      chapter_image_link,
+      image_number,
+      chapter_endpoint,
+      download_link;
+    chapter_endpoint = slug + "/";
+    content.find(".dsk2").filter(function () {
+      title = $(this).find("h1").text().replace("Komik ", "");
+    });
+    download_link = link;
+    content.find(".bc").filter(function () {
+      $(this)
+        .find("img")
+        .each(function (i, el) {
+          chapter_image_link = $(el).attr("src");
+          image_number = i + 1;
+          chapter_image.push({ image_number, chapter_image_link });
+        });
+    });
+    res.json({ title, chapter_endpoint, download_link, chapter_image });
   } catch (error) {
     res.send({ message: error });
   }
-
-  
 });
 
 module.exports = router;
